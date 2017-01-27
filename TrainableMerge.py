30f58df72385c0e5178f41160692edb066fccddf
Md Shopon <shopon.uap@gmail.com>
2017-01-27 22:00:48 +0600

    Weight Shape Updated

--- TrainableMerge.py
+++ TrainableMerge.py
@@ -6,33 +6,37 @@ from keras import initializations
 
 class TrainableMerge(Layer):
     def __init__(self, output_dim, input_dim,**kwargs):
-        init = 'uniform'
+        init = 'one'
         self.output_dim = output_dim
         self.input_dim= input_dim
         self.init = initializations.get(init)
+        print(input_dim)
         if self.input_dim:
             kwargs['input_shape'] = (self.input_dim,)
         super(TrainableMerge, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.W1 = self.add_weight(shape=(input_shape[0], self.output_dim),
+        self.W1 = self.add_weight(shape=(1,1),
                                  initializer=self.init,
                                  trainable=True)
-        self.W2 = self.add_weight(shape=(input_shape[1], self.output_dim),
+        self.W2 = self.add_weight(shape=(1, 1),
                                   initializer=self.init,
                                   trainable=True)
-        super(TrainableMerge, self).build()
     def call(self, x, mask=None):
         e1=x[0]
         e2=x[1]
         ''' Dot Product Final_Result=X1*W1+X2*W2'''
         Tensor_1=K.dot(e1, self.W1)
         Tensor_2=K.dot(e2,self.W2)
+        print(TrainableMerge.get_output_shape_for(self,Tensor_1))
+        print(TrainableMerge.get_output_shape_for(self,Tensor_2))
+
         inputs=[Tensor_1,Tensor_2]
-        result= K.sum(K.concatenate(inputs,axis=self.concat_axis))
+        result = inputs[0]
+        for i in range(0, 2):
+            result += inputs[i]
+        print(TrainableMerge.get_output_shape_for(self,result))
         return result
 
     def get_output_shape_for(self, input_shape):
-        print (input_shape)
-        batch_size = input_shape[0][0]
-        return (batch_size, self.output_dim)
+        return (input_shape[0], self.output_dim)
