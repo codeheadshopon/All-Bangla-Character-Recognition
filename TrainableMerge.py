9df690d8f47b9399fa0c5c5e9e2f53377699afcb
Md Shopon <shopon.uap@gmail.com>
2017-02-03 11:34:38 +0600

    Update TrainableMerge.py

--- TrainableMerge.py
+++ TrainableMerge.py
@@ -3,39 +3,35 @@ from keras.engine.topology import Layer
 import numpy as np
 import functools
 from keras import initializations
-
-class TrainableMerge(Layer):
+import theano.tensor as T
+class CustomMerge(Layer):
     def __init__(self, output_dim, input_dim,**kwargs):
-        init = 'one'
+        init = 'glorot_uniform'
         self.output_dim = output_dim
         self.input_dim= input_dim
         self.init = initializations.get(init)
-        print(input_dim)
         if self.input_dim:
             kwargs['input_shape'] = (self.input_dim,)
-        super(TrainableMerge, self).__init__(**kwargs)
+        super(CustomMerge, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.W1 = self.add_weight(shape=(1,1),
-                                 initializer=self.init,
-                                 trainable=True)
-        self.W2 = self.add_weight(shape=(1, 1),
-                                  initializer=self.init,
-                                  trainable=True)
+        # self.W1 = self.add_weight(shape=(input_shape[0], self.output_dim),
+        #                          initializer=self.init,
+        #                          trainable=True)
+        # self.W2 = self.add_weight(shape=(input_shape[1], self.output_dim),
+        #                           initializer=self.init,
+        #                           trainable=True)
+
+        self.W1=T.constant(0.5)
+        self.W2=T.constant(0.5)
+        # super(CustomMerge, self).build()
     def call(self, x, mask=None):
         e1=x[0]
         e2=x[1]
         ''' Dot Product Final_Result=X1*W1+X2*W2'''
         Tensor_1=K.dot(e1, self.W1)
         Tensor_2=K.dot(e2,self.W2)
-        print(TrainableMerge.get_output_shape_for(self,Tensor_1))
-        print(TrainableMerge.get_output_shape_for(self,Tensor_2))
-
-        inputs=[Tensor_1,Tensor_2]
-        result = inputs[0]
-        for i in range(0, 2):
-            result += inputs[i]
-        print(TrainableMerge.get_output_shape_for(self,result))
+        result= K.sum(K.concatenate([Tensor_1, Tensor_2]))
         return result
 
     def get_output_shape_for(self, input_shape):
