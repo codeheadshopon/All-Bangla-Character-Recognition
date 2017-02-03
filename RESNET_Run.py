53ffe59f28ee30e58a372b885605a287368ee91a
Md Shopon <shopon.uap@gmail.com>
2017-02-03 11:34:23 +0600

    Theano Tensor Constant

--- RESNET_Run.py
+++ RESNET_Run.py
@@ -30,15 +30,13 @@ from keras.callbacks import TensorBoard
 import cv2
 import numpy as np
 import gzip,cPickle,sys
-from resnet import Residual
 from skimage.transform import resize
 import scipy
 
 from keras.layers import BatchNormalization,merge
 from keras import backend as K
 # from TraunableMerge import NeuralTensorLayer as MERGE
-from TrainableMerge import TrainableMerge as MERGE
-
+from CustomMerge import CustomMerge as MERGE
 
 
 
@@ -47,7 +45,7 @@ from TrainableMerge import TrainableMerge as MERGE
 def identity_block(input_tensor, kernel_size, filters, stage, block):
 
     nb_filter1, nb_filter2, nb_filter3 = filters
-    if K.image_dim_ordering() == 'th':
+    if K.image_dim_ordering() == 'tf':
         bn_axis = 3
     else:
         bn_axis = 1
@@ -71,18 +69,13 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     #x = merge([x, input_tensor], mode='sum')
     ''' Calling Trainable Merge'''
     x = MERGE(output_dim=(1,28,28), input_dim=(1,28,28))([x,input_tensor])
-
-    # Tensor_1=Flatten()(x)
-    # Tensor_2=Flatten()(input_tensor)
-    # x = MERGE(output_dim=784, input_dim=784)([Tensor_1, Tensor_2])
-
     x = Activation('relu')(x)
     return x
 
 
 batch_size = 6
-# nb_classes = 10
-nb_classes = 231
+nb_classes = 10
+# nb_classes = 231
 nb_epoch = 100
 
 img_rows, img_cols = 28, 28
@@ -105,7 +98,10 @@ def dataset_load(path):
     return data
 
 (X_train,y_train),(X_test,y_test)=dataset_load('./BanglaFUll.pkl.gz')
-
+X_train=X_train[:100]
+y_train=y_train[:100]
+X_test=X_test[6:10]
+y_test=y_test[6:10]
 
 X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
 X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
@@ -114,7 +110,6 @@ X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 print('X_train shape:', X_train.shape)
-print('X_test shape:', X_test.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
@@ -154,13 +149,13 @@ CONV_2 = Convolution2D(16, kernel_size[0], kernel_size[1],
 
 resnet = CONV_2
 '''Residual Network '''
+
 Filters=[4,4,4]
 resnet=identity_block(resnet,3,Filters,1,'a')
 
-flat = Flatten()(resnet)
+flat=(resnet)
 dropout = Dropout(0.5)(flat)
 softmax = Dense(nb_classes, activation='softmax')(dropout)
-
 
 model = Model(input=[input_var], output=[softmax])
 model.compile(loss='categorical_crossentropy',
@@ -168,8 +163,12 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=2, validation_data=(X_test, Y_test))
+          verbose=1, validation_data=(X_test, Y_test))
 model.save('mnist_model.h5')
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+
+
+def get_output_shape_for(self, input_shape):
+    return (input_shape[0], self.output_dim)
