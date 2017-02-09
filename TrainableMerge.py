a7004e9e1a1ad69a61a05c1c0ae94adcba969a81
Md Shopon <shopon.uap@gmail.com>
2017-02-09 23:03:51 +0600

    Trainable Merge Updated With Single Layer

--- TrainableMerge.py
+++ TrainableMerge.py
@@ -6,27 +6,31 @@ from keras import initializations
 import theano.tensor as T
 from theano import function
 import theano
+from keras import regularizers,constraints
 class CustomMerge(Layer):
-    def __init__(self, output_dim, input_dim,**kwargs):
-        init = 'zero'
+    def __init__(self, output_dim, input_dim=None,**kwargs):
+        init = 'uniform'
+        W_regularizer = None
+        W_constraint = None,
         self.output_dim = output_dim
         self.input_dim= input_dim
-        self.init = initializations.get(init)
+        # self.init = initializations.get(init)
+        dim_ordering = K.image_dim_ordering()
+        self.init = initializations.get(init, dim_ordering=dim_ordering)
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.W_constraint = constraints.get(W_constraint)
         if self.input_dim:
             kwargs['input_shape'] = (self.input_dim,)
         super(CustomMerge, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.W1 = self.add_weight(shape=(input_shape[0], self.output_dim),
-                                 initializer=self.init,
-                                 trainable=True)
-        # self.x=theano.shared(np.array([1],'float32'),'x')
+        self.W=theano.shared(np.array(0.5,'float32'),'x')
 
-        super(CustomMerge, self).build()
+        # super(CustomMerge, self).build()
     def call(self, x, mask=None):
-        e1=x[0]
-        ''' Dot Product Final_Result=X1*W1+X2*W2'''
-        Tensor_1=K.dot(e1, self.W1)
+        e1=x
+        # Tensor_1=K.dot(e1, self.W)
+        Tensor_1=e1*self.W
         return Tensor_1
 
     def get_output_shape_for(self, input_shape):
